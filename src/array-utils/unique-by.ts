60d9a0e28e7fc38832fc51c31c7262fd5c794646
sebastian-schuler <24754485+sebastian-schuler@users.noreply.github.com>
2024-04-04 14:18:21 +0200

    fix: resolved type any eslint error

--- src/array-utils/unique-by.ts
+++ src/array-utils/unique-by.ts
@@ -1,22 +1,6 @@
-// /**
-//  * Type for a function that returns a key from an object.
-//  */
-// export type UniqueByKeyFn<T> = (item: T) => string;
+export type KeyExtractor<T, K> = (item: T) => K;
 
-// /**
-//  * Groups an array of objects by a key.
-//  * @param array - The array of objects to group
-//  * @param keyFunc - The function that returns the key to group by
-//  * @returns An object with the keys as the group by key and the values as the grouped objects
-//  */
-// export function uniqueBy<T>(array: T[], keyFunc: UniqueByKeyFn<T>): T[] {
-//   const arrayUniqueByKey = [...new Map(array.map((item) => [item[keyFunc(item)], item])).values()];
-//   return arrayUniqueByKey;
-// }
-
-export type KeyExtractor<T> = (item: T) => any;
-
-export function uniqueBy<T>(array: T[], keyFunc: KeyExtractor<T>): T[] {
+export function uniqueBy<T, K>(array: T[], keyFunc: KeyExtractor<T, K>): T[] {
   const uniqueKeys = new Set();
   return array.filter((item) => {
     const key = keyFunc(item);
